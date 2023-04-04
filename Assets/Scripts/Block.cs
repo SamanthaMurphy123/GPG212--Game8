e35197b3c24c07a3a183ef1fc754dbce1d565556
SamanthaMurphy123 <107165857+SamanthaMurphy123@users.noreply.github.com>
2023-04-04 20:12:43 +1000

    Initial commit

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BlockColor
+{
+    Green,
+    Red
+}
+public class Block : MonoBehaviour
+{
+    public BlockColor color;
+    public GameObject brokenBlockLeft;
+    public GameObject brokenBlockRight;
+
+    public float brokenBlockForce;
+    public float brokenBlockTorque;
+    public float brokenBlockDestroyDelay;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SwordRed"))
+        {
+            if(color == BlockColor.Red)
+            {
+                //add to score
+            }
+            else
+            {
+                //subtract life
+            }
+
+            Hit();
+        }
+        else if(other.CompareTag("SwordGreen"))
+        {
+            if (color == BlockColor.Green)
+            {
+                //add to score
+            }
+            else
+            {
+                //subtract life
+            }
+            Hit();
+        }
+    }
+
+    void Hit()
+    {
+        //enable broken pieces
+        brokenBlockLeft.SetActive (true);
+        brokenBlockRight.SetActive(true);
+
+        //remove them as children
+        brokenBlockLeft.transform.parent = null;
+        brokenBlockRight.transform.parent = null;
+
+        //Add force to them
+        Rigidbody leftRig = brokenBlockLeft.GetComponent<Rigidbody>();
+        Rigidbody rightRig = brokenBlockRight.GetComponent<Rigidbody>();
+
+        leftRig.AddForce(-transform.right * brokenBlockForce, ForceMode.Impulse);
+        rightRig.AddForce(transform.right * brokenBlockForce, ForceMode.Impulse);
+
+        //Add torque to them
+        leftRig.AddTorque(-transform.forward * brokenBlockTorque, ForceMode.Impulse);
+        rightRig.AddTorque(transform.forward * brokenBlockTorque, ForceMode.Impulse);
+
+        //Destroy pieces after few secs
+        Destroy(brokenBlockLeft, brokenBlockDestroyDelay);
+        Destroy(brokenBlockRight, brokenBlockDestroyDelay);
+
+        //Destroy mmain block
+        Destroy(gameObject);
+
+    }
+}
